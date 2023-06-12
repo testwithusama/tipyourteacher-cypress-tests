41c64b4f26455c1bd2e001ebf707a2888570fd22
Shaikh Usama Bin Naeem <usamanjc@gmail.com>
2023-06-12 14:28:50 +0500

    Test: Updated the target URL to staging

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,7 +1,20 @@
 describe("Dashboard Tests", () => {
   it("Should be able to land on the login page", () => {
-    cy.visit("https://tipyourteacher-csvj1c7qr-desolintdev.vercel.app/");
+    cy.visit("https://tipyourteacher-staging.vercel.app");
+    cy.get(".get-started-default-input").type("testing");
+    cy.get(".get-started-default-input-button").click();
+    cy.get(".justify-content-between > :nth-child(1)").should(
+      "have.text",
+      "Email/Password"
+    );
+    cy.get(".justify-content-between > :nth-child(1)").click();
+    cy.get(":nth-child(2) > #formEmail").type("usamatesteng@gmail.com");
+    cy.get(":nth-child(3) > .input-group > #formpassword").type("Test@1998");
+    cy.get(":nth-child(4) > .input-group > #formpassword").type("Test@1998");
     cy.get(".login100-form-btn").click();
-    cy.get(".dark-layout > .fe").click();
+    cy.get(".bg-white").should(
+      "have.text",
+      "get an email from us soon, when we will go live."
+    );
   });
 });
